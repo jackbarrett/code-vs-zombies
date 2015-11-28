4eaf535eaf50ca627715fa9ddbc9d16dd3045a72
Charles Barrette <cfbarrette@gmail.com>
2015-11-28 18:12:25 -0500

    Point entity

--- entities.js
+++ entities.js
@@ -10,12 +10,12 @@ function Zombie() {
     id: parseInt(inputs[0]),
     pos: new Point(parseInt(inputs[1]), parseInt(inputs[2])),
     dest: new Point(parseInt(inputs[3]), parseInt(inputs[4]))
-    };
-  }
+  };
+}
 
-  function Human() {
-    return {
-      id: parseInt(inputs[0]),
-      pos: new Point(parseInt(inputs[1]), parseInt(inputs[2])),
-    };
-  }
+function Human() {
+  return {
+    id: parseInt(inputs[0]),
+    pos: new Point(parseInt(inputs[1]), parseInt(inputs[2])),
+  };
+}
